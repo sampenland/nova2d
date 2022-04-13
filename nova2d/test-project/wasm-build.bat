3cb266722647dfde82aa4eb40db3b0ce84e3e10a
Sam <sampenland@gmail.com>
2022-04-13 16:18:36 -0400

    tried to link with lib - didnt work

--- nova2d/test-project/wasm-build.bat
+++ nova2d/test-project/wasm-build.bat
@@ -1,5 +1,2 @@
-set p1=../nova-zero/src
-set p2=../Dependencies/mysql/Release/include
-set p3=../Dependencies/box2d/include
 
-emcc .\src\main.cpp -O2 -s USE_SDL=2 -s USE_SDL_TTF=2 -s USE_SDL_MIXER=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -I%p1% -I%p2% -I%p3% --preload-file res -o test-project.html
+emcc .\src\main.cpp ..\nova-zero\web-dist\nova2d-lib.a -O2 -s USE_SDL=2 -s USE_SDL_TTF=2 -s USE_SDL_MIXER=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --preload-file res -o test-project.html
