82d0f3eea8023c971773c3d63adccafd87fb2d00
Sam <sampenland@gmail.com>
2022-04-21 09:52:31 -0400

    update to director

--- nova2d/nova-zero/src/utils/Director.cpp
+++ nova2d/nova-zero/src/utils/Director.cpp
@@ -17,27 +17,30 @@ namespace novazero
 		{
 			ImGui::Begin("Director");
 
-			ImGui::Text("Running: %.3f ms/frame (%.1f FPS)",
-				1000.0 / float(ImGui::GetIO().Framerate), float(ImGui::GetIO().Framerate));
-			
-			if (Game::s_SceneManager->GetCurrentScene())
-			{
-				if (Game::s_SceneManager->GetCurrentScene()->m_PhysicsEnabled)
-				{
-					ImGui::Checkbox("Debug Physics: ", &Game::s_SceneManager->GetCurrentScene()->m_DebugDraw);
-				}
-			}
+			ImGui::SliderFloat("Timescale", &Game::s_TimeScale, 0.1f, 6.f);
 
 			ImGui::Text("Drawables: %d", n2dDebugDrawableCount());
 			ImGui::Text("Deleteables: %d", n2dDebugDeleteableCount());
 			ImGui::Text("Updaters: %d", n2dDebugUpdaterCount());
 			ImGui::Text("References: %d", n2dDebugReferencedCount());
 			ImGui::Text("Tweens: %d", n2dDebugTweenCount());
-			
-			ImGui::SliderFloat("Timescale", &Game::s_TimeScale, 0.1f, 6.f);
+
+			if (Game::s_SceneManager->GetCurrentScene())
+			{
+				if (Game::s_SceneManager->GetCurrentScene()->m_PhysicsEnabled)
+				{
+					ImGui::Checkbox("Debug Physics", &Game::s_SceneManager->GetCurrentScene()->m_DebugDraw);
+				}
+
+				ImGui::Checkbox("Debug Log", &Game::s_Debug);
+				ImGui::Checkbox("Debug Verbose Log", &Game::s_DebugVerbose);
+			}
 			
 			if(m_LightingSlider)
 				m_LightingSlider->Draw();
+
+			ImGui::Text("Running: %.3f ms/frame (%.1f FPS)",
+				1000.0 / float(ImGui::GetIO().Framerate), float(ImGui::GetIO().Framerate));
 			
 			ImGui::End();
 		}
